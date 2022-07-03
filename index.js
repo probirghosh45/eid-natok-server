95af43b662d27b1c796dfaae153b4d87a64f0843
probir ghosh <probirghosh@outlook.com>
2022-07-03 11:49:01 +0600

    cors policy issue fixed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const app = express()
-const { MongoClient } = require('mongodb');
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
 // import { MongoClient } from "mongodb";
-require('dotenv').config()
-const port = 4700 || process.env.PORT ;
+require("dotenv").config();
+const cors = require('cors')
+const port = 4700 || process.env.PORT;
+
+
+// middleware
+
+app.use(cors())
 
 
 
@@ -11,23 +17,34 @@ const port = 4700 || process.env.PORT ;
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.ywri3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-app.get('/', (req, res) => {
-  res.send("What's UP Bro🥴!!!")
-})
+app.get("/", (req, res) => {
+  res.send("What's UP Bro🥴!!!");
+});
 
 async function run() {
-    try {
-      await client.connect();
+  try {
+    await client.connect();
     //   console.log("DB Connected😁");
-      const natokCollection = client.db("eid_natok2022").collection("natok");;
+    const natokCollection = client.db("eid_natok2022").collection("natok");
 
-    } finally {
+ 
+    // POST method route
+    app.post("/eid-natok", (req, res) => {
+      res.send("POST request to the homepage");
+    });
+
+       // GET method route
+       app.get("/eid-natok-collection", (req, res) => {
+    //    console.log(req);
+        res.send("Hello Bhola!!!");
+      });
+
+  } finally {
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
-
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
